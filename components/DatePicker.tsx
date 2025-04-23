aafab824b0c8a480d72e1adddddbb9ca8f433de1
TheRaulH <raulherreracruz18@gmail.com>
2025-04-23 17:51:52 -0400

    feat: Implement DatePicker component for date selection with formatted output

---
+++ components/DatePicker.tsx
@@ -0,0 +1,55 @@
+"use client";
+
+import * as React from "react";
+import { format, parseISO } from "date-fns";
+import { CalendarIcon } from "lucide-react";
+
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import { Calendar } from "@/components/ui/calendar";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+
+interface DatePickerProps {
+  value: string | undefined; // Cambiado a string | undefined
+  onChange: (dateString: string | undefined) => void; // Cambiado a string | undefined
+}
+
+export function DatePicker({ value, onChange }: DatePickerProps) {
+  const [date, setDate] = React.useState<Date | undefined>(
+    value ? parseISO(value) : undefined
+  );
+
+  const handleDateChange = (date: Date | undefined) => {
+    setDate(date);
+    onChange(date ? format(date, "yyyy-MM-dd") : undefined);
+  };
+
+  return (
+    <Popover>
+      <PopoverTrigger asChild>
+        <Button
+          variant={"outline"}
+          className={cn(
+            "w-full justify-start text-left font-normal",
+            !date && "text-muted-foreground"
+          )}
+        >
+          <CalendarIcon className="mr-2 h-4 w-4" />
+          {date ? format(date, "dd/MM/yyyy") : <span>Seleccionar fecha</span>}
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="w-auto p-0">
+        <Calendar
+          mode="single"
+          selected={date}
+          onSelect={handleDateChange}
+          initialFocus
+        />
+      </PopoverContent>
+    </Popover>
+  );
+}
