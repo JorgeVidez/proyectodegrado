a2f0496fc03ba872a75bb47cd6b0a400908b6e66
TheRaulH <raulherreracruz18@gmail.com>
2025-05-20 14:54:53 -0400

    feat: integrate next-pwa for improved PWA support and configuration

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,27 @@
 import type { NextConfig } from "next";
+import withPWA from "next-pwa";
 
-const nextConfig: NextConfig = {
+const nextConfig = {
   /* config options here */
+  distDir: "build",
+  reactStrictMode: true,
+  compiler: { removeConsole: process.env.NODE_ENV === "production" },
 };
 
-export default nextConfig;
+// Configuración del plugin next-pwa
+const pwaOptions = {
+  dest: "public", // Directorio donde se generará el Service Worker (sw.js, workbox-*.js)
+  // Deshabilitar PWA en modo de desarrollo.
+  // Esto evita que el Service Worker cachee recursos y cause problemas
+  // con la recarga en caliente (hot reloading) o cambios de código durante el desarrollo.
+  // La PWA se habilitará automáticamente cuando construyas para producción (NODE_ENV='production').
+  disable: process.env.NODE_ENV === "development", // <-- Cambiado a 'development'
+
+  register: true, // Registra automáticamente el Service Worker
+  skipWaiting: true, // Instala el Service Worker inmediatamente, útil para actualizaciones rápidas
+
+  // Agrega otras opciones de next-pwa si las necesitas (por ejemplo, runtimeCaching)
+};
+
+// Aplica el withPWA directamente a la configuración
+export default withPWA(pwaOptions)(nextConfig);
